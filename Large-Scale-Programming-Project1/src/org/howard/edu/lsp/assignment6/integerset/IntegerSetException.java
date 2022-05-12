e24f636fe49bd7633eb43f952bd1315dc4a512a1
benjin8or <benjin8or@localhost>
2022-05-12 13:40:38 -0400

    Assignment 6

---
+++ Large-Scale-Programming-Project1/src/org/howard/edu/lsp/assignment6/integerset/IntegerSetException.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment6.integerset;
+
+public class IntegerSetException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IntegerSetException(String errorMessage) {
+		super(errorMessage);
+		}
+    }
+
